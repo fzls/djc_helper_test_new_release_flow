ab00384b3b993852b8be8d751795761e7748e3b3
风之凌殇 <fzls.zju@gmail.com>
2022-08-22 17:45:39 +0800

    feat: 移除更新内容中固定的部分

--- _gen_changelog_for_github_release.py
+++ _gen_changelog_for_github_release.py
@@ -10,6 +10,24 @@ from util import make_sure_dir_exists
 def gen_changelog():
     update_info = get_update_info_from_local_file("README.MD", "CHANGELOG.MD")
 
+    logger.info("移除末尾固定的两个更新条目")
+    fixed_items = [
+        "如需购买自动更新DLC或按月付费，请在配置工具的【付费相关】标签页自助购买，具体流程请查阅目录中【付费指引.docx】",
+        "其他改动及上述功能具体用法，详见README.MD和CHANGELOG.MD以及使用教程/使用文档.docx和教程视频",
+    ]
+    message_list = []
+    for message in update_info.update_message.split("\n"):
+        filtered = False
+        for item in fixed_items:
+            if item in message:
+                filtered = True
+                break
+
+        if not filtered:
+            message_list.append(message)
+
+    update_info.update_message = "\n".join(message_list)
+
     github_release_dir = os.path.realpath("./releases/_github_action_artifact")
     make_sure_dir_exists(github_release_dir)
 
